ecfdf354a17fb14255ffa7c8161f480ae864e78d
luizpetty <luiz.h.petty@gmail.com>
2024-09-13 20:29:18 -0300

    maquina de estado finito

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int transicao,estado;
+
+Console.WriteLine("Digite a Estado inserindo 0 ou 1");
+transicao = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o Transição inserindo 0 ou 1");
+estado = int.Parse(Console.ReadLine());
+
+if (estado == 1 & transicao == 0 | estado == 2 & transicao == 0)
+{
+    Console.WriteLine("Seu estado Atual é 3 ");
+}
+else if(estado == 3 & transicao == 1)
+{
+    Console.WriteLine("Seu estado Atual é 2 ");
+}
+else if(estado == 2 & transicao == 1 & transicao == 1)
+{
+    Console.WriteLine("Seu estado Atual é 1" );
+}
+else if(estado == 2 & transicao == 0)
+{
+    estado = 3;
+}
+
+
